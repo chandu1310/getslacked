82ae469c1044ee1efb49ff2d9005ab6e05612873
Chandra Shekar Chennamsetty <chandra.shekar.chennamsetty@autodesk.com>
2018-09-09 03:20:27 -0700

    feat(Incoming): Implemented message listener for direct and channel messages

--- src/SlackMessageListener.js
+++ src/SlackMessageListener.js
@@ -1,11 +1,11 @@
+const EventEmitter = require('events');
 const { RTMClient } = require('@slack/client');
 const SlackMessageDispatcher = require('./SlackMessageDispatcher');
 
 class SlackMessageListener {
   constructor(token){
     this.token = token;
-    this.channels = new Map();
-    this.messagelisteners = new Map();
+    this.slackevents = new EventEmitter();
     this.slack = new SlackMessageDispatcher(this.token);
     const rtm = new RTMClient(token);
     this.rtm = rtm;
@@ -16,15 +16,14 @@ class SlackMessageListener {
     this.rtm.on('ready', () => {
       console.debug(`SlackMessageListener.constructor: Ready to receive RTM.`);
       this.rtm.on(`message`, (message) => {
-        console.log(message)
         if(message.type==='message' && message.channel && message.user){
           this.getUser(message.user)
           .then(user => {
             if(user){
               const channel = {
-                isDirect: !this.channels.has(message.channel),
+                isDirect: !this.slack.channels.has(message.channel),
                 id: message.channel,
-                name: this.channels.get(message.channel)
+                name: this.slack.channels.get(message.channel)
               };
               this.processMessage({
                 channel: channel,
@@ -39,7 +38,40 @@ class SlackMessageListener {
   }
 
   processMessage(message){
-    console.debug(`SlackMessageListener.processMessage: Processing incoming message.`, message);
+    console.debug(`SlackMessageListener.processMessage: Processing incoming message.`);
+    let eventName = `USER/${message.from.displayname.toUpperCase()}`;
+    if(!message.channel.isDirect) {
+      // For INDIRECT messages
+      // eventName = `USER/${message.from.displayname.toUpperCase()}/CHANNEL/${message.channel.name.toUpperCase()}`;
+      // console.debug(`Sending event: ${eventName}`);
+      // this.slackevents.emit(eventName, message);
+
+      eventName = `CHANNEL/${message.channel.name.toUpperCase()}`;
+      // console.debug(`Sending event: ${eventName}`);  
+      this.slackevents.emit(eventName, message);
+    } else {
+      // For DIRECT messages
+      // console.debug(`Sending event: ${eventName}`);
+      this.slackevents.emit(eventName, message);
+    }
+  }
+
+  onMessageInChannel(channelname, callback){
+    const topic = `CHANNEL/${channelname.toUpperCase()}`;
+    // console.debug(`Registering for ${topic}`);
+    this.slackevents.on(topic, callback);
+  }
+
+  // onIndirectMessageFrom(sender, channelname, callback){
+  //   const topic = `USER/${sender.toUpperCase()}/CHANNEL/${channelname.toUpperCase()}`;
+  //   // console.debug(`Registering for ${topic}`);
+  //   this.slackevents.on(topic, callback);
+  // }
+
+  onDirectMessageFrom(sender, callback){
+    const topic = `USER/${sender.toUpperCase()}`;
+    // console.debug(`Registering for ${topic}`);
+    this.slackevents.on(topic, callback);
   }
 
   getUser(name){
